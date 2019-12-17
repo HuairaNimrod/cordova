1183ece3afc9e11367296aac09c09ccf0b677807
nimrodhuaira <nimrod.huaira@entel.pe>
2019-12-17 16:55:01 -0500

    nmx

--- www/toastyplugin.js
+++ www/toastyplugin.js
@@ -1,21 +1,22 @@
+
 // Empty constructor
-function cordova() {}
+function ToastyPlugin() {}
 
 // The function that passes work along to native shells
 // Message is a string, duration may be 'long' or 'short'
-cordova.prototype.show = function(message, duration, successCallback, errorCallback) {
+ToastyPlugin.prototype.show = function(message, duration, successCallback, errorCallback) {
   var options = {};
   options.message = message;
   options.duration = duration;
-  cordova.exec(successCallback, errorCallback, 'cordova', 'show', [options]);
+  cordova.exec(successCallback, errorCallback, 'ToastyPlugin', 'show', [options]);
 }
 
 // Installation constructor that binds ToastyPlugin to window
-cordova.install = function() {
+ToastyPlugin.install = function() {
   if (!window.plugins) {
     window.plugins = {};
   }
-  window.plugins.cordova = new cordova();
-  return window.plugins.cordova;
+  window.plugins.toastyPlugin = new ToastyPlugin();
+  return window.plugins.toastyPlugin;
 };
-cordova.addConstructor(cordova.install);
+cordova.addConstructor(ToastyPlugin.install);
